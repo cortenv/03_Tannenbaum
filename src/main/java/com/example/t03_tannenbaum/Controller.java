edf2f3136c335fd04ff0f1a4621410ea8af647f5
cortenv <vera.corten@hotmail.com>
2022-12-02 08:46:19 +0100

    first push

--- src/main/java/com/example/t03_tannenbaum/Controller.java
+++ src/main/java/com/example/t03_tannenbaum/Controller.java
@@ -1,54 +1,41 @@
 package com.example.t03_tannenbaum;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.text.Text;
+
 
 public class Controller {
+
+    private Tanne Tanne;
+
     @FXML
     private TextField Stammbreite;
 
     @FXML
-    private TextField Stammhöhe;
+    private TextField Stammhoehe;
 
     @FXML
-    private TextField Kronenhöhe;
+    private TextField Kronenhoehe;
 
     @FXML
-    private Button buttonMath;
+    private TextArea areaZeichnung;
 
     @FXML
-    private TextArea areaZeichung;
-
-    @FXML
-    private void onButtonDrawClick() {
-
-        if (Kronenhöhe.getText().trim().equals("")
-                || Stammhöhe.getText().equals("")
+    private void buttonMath() {
+        if (Kronenhoehe.getText().trim().equals("")
+                || Stammhoehe.getText().equals("")
                 || Stammbreite.getText().equals("")) {
             return;
         }
 
-// Aus den Lösungen kopiert
-        Tanne t = new Tanne(
-                Integer.valueOf(Stammbreite.getText()) ,
-                Integer.valueOf(Stammhöhe.getText()) ,
-                Integer.valueOf(Kronenhöhe.getText()));
 
-        t.Zeichne();
+        Tanne t = new Tanne(Integer.valueOf(Stammbreite.getText())
+                , Integer.valueOf(Stammhoehe.getText())
+                , Integer.valueOf(Kronenhoehe.getText()));
 
-        areaZeichung.setText(t.getZeichnung());
+        t.zeichne();
+
+        areaZeichnung.setText(t.getZeichnung());
     }
-
-    @FXML
-    private void initialize() {
-        Stammbreite.addEventFilter(KeyEvent.ANY, keyEventFilter);
-        Stammhöhe.addEventFilter(KeyEvent.ANY, keyEventFilter);
-        Kronenhöhe.addEventFilter(KeyEvent.ANY, keyEventFilter);
-    }
-
 }
--- src/main/java/com/example/t03_tannenbaum/Tanne.java
+++ src/main/java/com/example/t03_tannenbaum/Tanne.java
@@ -5,41 +5,53 @@ public class Tanne {
     private int Stammbreite;
     private int Stammhoehe;
     private int Kronenhoehe;
+    private String Zeichnung;
 
-    private String Zeichnung = "";
-
-    public int getStammbreite() {
-        return Stammbreite;
+    public void setStammhoehe(int stammhoehe) {
+        this.Stammhoehe = stammhoehe;
     }
 
     public int getStammhoehe() {
         return Stammhoehe;
     }
 
+    public void setStammbreite(int stammbreite) {
+        this.Stammbreite = stammbreite;
+    }
+
+    public int getStammbreite() {
+        return Stammbreite;
+    }
+
+    public void setKronenhoehe(int kronenhoehe) {
+        this.Kronenhoehe = kronenhoehe;
+    }
+
     public int getKronenhoehe() {
         return Kronenhoehe;
     }
+
+
 
     public String getZeichnung() {
         return Zeichnung;
     }
 
-    public void setStammbreite(int stammbreite) {
-        Stammbreite = stammbreite;
+    public void setZeichnung(String zeichnung) {
+        this.Zeichnung = zeichnung;
     }
 
-    public void setStammhoehe(int stammhoehe) {
-        Stammhoehe = stammhoehe;
+    public Tanne(int stammbreite, int stammhoehe, int kronenhoehe) {
+        this.Stammbreite = stammbreite;
+        this.Stammhoehe = stammhoehe;
+        this.Kronenhoehe = kronenhoehe;
     }
 
-    public void setKronenhoehe(int kronenhoehe) {
-        Kronenhoehe = kronenhoehe;
-    }
+    public void zeichne() {
+        Zeichnung = "";
+        zeichneKrone();
+        zeichneStamm();
 
-    public void Zeichne() {
-        String Zeichnung = "";
-        String ZeichneKrone;
-        String ZeichneStamm;
     }
 
     private void zeichneKrone() {
@@ -52,12 +64,12 @@ public class Tanne {
             zeichneZeile(Kronenhoehe - Stammbreite / 2 - 1, Stammbreite);
     }
 
-    private void zeichneZeile(int Anfang, int Breite) {
+    private void zeichneZeile(int start, int breite) {
         int i;
-        for (i = 1; i <= Anfang; ++i)
+        for (i = 1; i <= start; ++i)
             Zeichnung += " ";
 
-        for (i = 1; i <= Breite; ++i)
+        for (i = 1; i <= breite; ++i)
             Zeichnung += "*";
 
         Zeichnung += "\n";
